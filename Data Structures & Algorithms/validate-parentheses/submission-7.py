4b9a2224633156d2a80044c251458a93f892044f
avpreet <115603384+Avpreetkaur@users.noreply.github.com>
2026-08-16 13:07:19 +0530

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        #create a dic to map
+        pairs = {
+            ")":"(",
+            "]":"[",
+            "}":"{"
+        }
+        stack = []
+        #loop through characters and see if char in pair's values 
+        for char in s:
+            if char in "([{":
+                stack.append(char) #"["("]
+            else:
+                if not stack:
+                    return False
+                if pairs[char] == stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+        return not stack
